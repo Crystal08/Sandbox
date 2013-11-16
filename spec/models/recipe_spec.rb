697bf6a69adc99d4035e86ef6a49f7d56aef41cc
Crystal08 <Crystal08@compuserve.com>
2013-11-16 08:24:40 -0600

    Restart

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Recipe do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it {should respond_to(:ingredients)}
+  it {should respond_to(:recipe_ingredients)}
 end
